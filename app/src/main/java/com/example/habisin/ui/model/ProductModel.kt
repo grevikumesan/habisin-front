60b51660a08cc79df297c2a7a3c0fc0e9b0e656b
jefereysantosa <jefereyteddy88888@gmail.com>
2026-05-08 01:38:48 +0700

    Repair Fridgee & Add Food View

--- app/src/main/java/com/example/habisin/ui/model/ProductModel.kt
+++ app/src/main/java/com/example/habisin/ui/model/ProductModel.kt
@@ -1,14 +1,15 @@
 package com.example.habisin.ui.model
 
-
+import java.util.Date
 
 // Representasi data bahan makanan di kulkas
 data class ProductModel(
     val id: String,
     val name: String,
     val category: String, // "Produce", "Dairy", "Meat", "Other"
-    val bestBeforeDate: String,
+    val bestBeforeDate: Date,
     val daysLeft: Int,
     val quantity: Int,
+    val unit: String,
     val imageUrl: String // Nullable karena bisa pakai default icon kalau ga ada foto
 )
