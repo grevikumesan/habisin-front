269b2d7536d8bae0c816047b4063962dd3fbb60e
chris <christoperjoel1@gmail.com>
2026-05-19 19:14:13 +0700

    Emoji muncul dalam bahasa indonesia

--- app/src/main/java/com/example/habisin/util/getProductEmoji.kt
+++ app/src/main/java/com/example/habisin/util/getProductEmoji.kt
@@ -2,18 +2,18 @@ package com.example.habisin.util
 
 fun getProductEmoji(name: String): String {
     return when {
-        name.contains("Egg",     ignoreCase = true) -> "🥚"
-        name.contains("Bread",   ignoreCase = true) -> "🥖"
-        name.contains("Apple",   ignoreCase = true) -> "🍎"
-        name.contains("Milk",    ignoreCase = true) -> "🥛"
-        name.contains("Carrot",  ignoreCase = true) -> "🥕"
-        name.contains("Spinach", ignoreCase = true) -> "🥬"
-        name.contains("Chicken", ignoreCase = true) -> "🍗"
-        name.contains("Fish",    ignoreCase = true) -> "🐟"
-        name.contains("Cheese",  ignoreCase = true) -> "🧀"
+        name.contains("Telur",     ignoreCase = true) -> "🥚"
+        name.contains("Roti",   ignoreCase = true) -> "🥖"
+        name.contains("Apel",   ignoreCase = true) -> "🍎"
+        name.contains("Susu",    ignoreCase = true) -> "🥛"
+        name.contains("Wortel",  ignoreCase = true) -> "🥕"
+        name.contains("Bayam", ignoreCase = true) -> "🥬"
+        name.contains("Ayam", ignoreCase = true) -> "🍗"
+        name.contains("Ikan",    ignoreCase = true) -> "🐟"
+        name.contains("Keju",  ignoreCase = true) -> "🧀"
         name.contains("Yogurt",  ignoreCase = true) -> "🥣"
-        name.contains("Banana",  ignoreCase = true) -> "🍌"
-        name.contains("Tomato",  ignoreCase = true) -> "🍅"
+        name.contains("Pisang",  ignoreCase = true) -> "🍌"
+        name.contains("Tomat",  ignoreCase = true) -> "🍅"
         else -> "📦"
     }
 }
